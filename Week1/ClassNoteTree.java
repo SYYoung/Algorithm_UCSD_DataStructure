f960ce53d86a0871418733e8d37b931617d9266a
Ki Ling Cheung <kilingcheung@Kis-MacBook-Pro.local>
2018-04-09 23:06:12 +0800

    add classnote: tree1

---
+++ Week1/ClassNoteTree.java
@@ -0,0 +1,15 @@
+package Week1;
+
+public class ClassNoteTree {
+	public void Height() {
+		//	the input argument should include tree
+		//	if tree== nil: return 0
+		//	return 1+max(Height(tree.left), Height(tree.right))
+	}
+	public void size() {
+		// the input arg should include tree
+		// if tree == nil
+		//		return 0
+		// 	return 1 + size(tree.left) + size(tree.right)
+	}
+}
