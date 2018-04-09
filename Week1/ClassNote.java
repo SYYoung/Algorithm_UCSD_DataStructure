3e7b6cf9c4f2c7fc59e6cf7d65059821c650fef5
Ki Ling Cheung <kilingcheung@Kis-MacBook-Pro.local>
2018-04-09 13:52:26 +0800

    initial commit

---
+++ Week1/ClassNote.java
@@ -0,0 +1,14 @@
+package Week1;
+
+public class ClassNote {
+	private class SingleLL {
+		public void PushFront(int key) {
+			// node <- new node
+			// node.key <- key
+			// node.next <- head
+			// head <- node
+			// if tail == nil:
+			//		tail <- head
+		}
+	}
+}
